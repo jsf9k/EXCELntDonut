231a022ccd8ee51bccdf6567462484bfac96448c
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-09-19 14:38:59 -0400

    Point to my fork of TheWover/donut
    This will no longer be necessary once TheWover/donut#158 is merged.

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ setup(
     author = 'Joseph Leon (FortyNorthSecurity.com)',
     author_email = 'jleon@fortynorthsecurity.com',
     packages = ['EXCELntDonut'],
-    install_requires = ['donut-shellcode','pandas'],
+    install_requires = [
+        'donut-shellcode @ https://github.com/jsf9k/donut/tarball/bugfix/python-extension-fails-to-build',
+        'pandas'
+      ],
     entry_points = {
         'console_scripts': [
             'EXCELntDonut = EXCELntDonut.drive:main'
